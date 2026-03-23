863302e424305fa18bfda559014969b6caf27730
Aziz Charles <taariqc2000@gmail.com>
2026-03-23 17:46:18 +0200

    feat: implement spacecraft orbital kinematics with pause/resume functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
 
+
+const fleet = []; // Keeps track of ships
+let isOrbiting = true; // Tracks if the game is paused
+let currentOrbitTime = 0; // Custom time tracker
 const canvas = document.querySelector("#c");
 
 const scene = new THREE.Scene();
@@ -15,6 +19,19 @@ camera.position.z = 40; // Move the camera out
 // Add the SpaceStation to the scene to be rendered
 const spaceStation = new THREE.Group();
 scene.add(spaceStation);
+
+// Create placeholder ships
+ for (let i = 0; i < 4; i++) {
+    const shipGeometry = new THREE.ConeGeometry(1, 3, 8);
+    const shipMaterial = new THREE.MeshBasicMaterial({color: "#fb8500", wireframe: true});
+    const ship = new THREE.Mesh(shipGeometry, shipMaterial);
+    
+    ship.rotation.x = Math.PI / 2;
+    scene.add(ship); // add to the scene, not spaceStation
+
+    // add Ship to the fleet array
+    fleet.push(ship);
+ }
 
 // Create the central core
 const centralCoreGeometry = new THREE.SphereGeometry(15, 32, 16);
@@ -87,9 +104,39 @@ window.addEventListener('resize', () => {
     // 3. Update renderer size
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
-
+ 
+// listens for the "p" key yo toggle the pause state
+window.addEventListener("keydown", e =>{
+    if (e.key.toLowerCase() === "p") {
+        isOrbiting = !isOrbiting; // Flips true to false, or false to true
+        console.log("Orbiting active:", isOrbiting); // Helpful for debugging
+    }
+});
 // render (animation loop). This will create a loop that causes the renderer to draw the scene every time the screen is refreshed 
 const animate = time => {
+
+    // increase our custom time if we are not paused
+    // (We add 16.6 milliseconds, which is roughly the time of one frame at 60FPS)
+    if (isOrbiting) {
+        currentOrbitTime += 16.6;
+    }
+    // Orbital physics for the fleet
+    const orbitSpeed = 0.0001; // How fast they fly
+    const orbitRadius = 60; // How far out they fly
+
+    fleet.forEach((ship, index) => {
+        // Add an offset so they don't all clump together (spaces them out by 90 degrees)
+        const angleOffset = (index / fleet.length) * Math.PI * 2;
+        const currentAngle = (currentOrbitTime * orbitSpeed) + angleOffset;
+
+        // calculate new position
+        ship.position.x = Math.cos(currentAngle) * orbitRadius;
+        ship.position.z = Math.sin(currentAngle) * orbitRadius;
+
+        // make the ship face the direction is is flying
+        // A circle's tangent is exactly 90 degrees (PI/2) ahead of its current angle
+        ship.rotation.y = -currentAngle;
+    });
     spaceStation.rotation.x = time / 2000;
     spaceStation.rotation.y = time / 1000;
 
